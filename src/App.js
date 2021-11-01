a825fb8b1cefcb3cc7d6b39b707897828895f716
Viktor-sun <bobick.zdoh@gmail.com>
2021-11-01 20:13:42 +0200

    feat: add app bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import AppBar from './components/AppBar';
 import CardsContainer from './components/CardsContainer';
 
 function App() {
   return (
     <div className="App">
+      <AppBar />
       <CardsContainer />
     </div>
   );

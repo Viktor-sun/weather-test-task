6791052ceb3f33bb8bfe7b79780c2ca57d4c2f87
Viktor-sun <bobick.zdoh@gmail.com>
2021-11-03 14:50:47 +0200

    fix: fix refresh city

--- src/redux/weather/weather-reducer.js
+++ src/redux/weather/weather-reducer.js
@@ -4,15 +4,25 @@ import weatherActions from './weather-actions';
 
 const citys = createReducer([], {
   [weatherActions.citysSuccess]: (state, { payload }) => [...state, payload],
-  // ????????????????????????????????????????//
   [weatherActions.refreshCitySuccess]: (state, { payload }) => {
-    const delOldInfo = state.filter(e => e.id !== payload.id);
-    return [...delOldInfo, payload];
+    const idx = state.findIndex(({ id }) => id === payload.id);
+    const newState = [...state];
+    newState.splice(idx, 1, payload);
+    return newState;
   },
   [weatherActions.delCity]: (state, { payload }) =>
     state.filter(e => e.id !== payload),
 });
 
+const error = createReducer(null, {
+  [weatherActions.citysError]: (_, { payload }) => payload,
+  [weatherActions.refreshCityError]: (_, { payload }) => payload,
+
+  [weatherActions.citysSuccess]: () => null,
+  [weatherActions.refreshCitySuccess]: () => null,
+});
+
 export default combineReducers({
   citys,
+  error,
 });
